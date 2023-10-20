32c152ffb9073992ed839517e963ee7fea29a30b
awalford16 <awalford16@gmail.com>
2023-10-20 17:31:23 +0100

    arm docker image

--- src/office-lights/main.py
+++ src/office-lights/main.py
@@ -4,32 +4,40 @@ import time
 
 hue = PhillipsHue()
 
+
 # Callback when a message is received from the broker
 def on_message(client, userdata, message):
     state = message.payload.decode()
     print(f"Setting state to {state}")
 
+    if state == "OFF":
+        hue.change_light_state(Groups.OFFICE, False)
+        return
+
     if not hasattr(States, state):
         print("Invalid State, will default to FOCUS")
-        state="FOCUS"
+        state = "FOCUS"
 
     hue.change_light_state(Groups.OFFICE, True, States[state])
-    time.sleep(100)
-    hue.change_light_state(Groups.OFFICE, False)
+
 
 if __name__ == "__main__":
-    mqtt = MQTT(on_message)
-
-    mqtt.subscribe("office/lights")
-    
-    # Start the network loop to process incoming and outgoing messages
-    mqtt.client.loop_start()
-
-    # Keep the program running to receive messages
     try:
+        mqtt = MQTT(on_message)
+
+        mqtt.subscribe("office/lights")
+
+        # Start the network loop to process incoming and outgoing messages
+        mqtt.client.loop_start()
+    except Exception:
+        print("MQTT Configuration failed")
+
+    try:
+        # Keep the program running to receive messages
         while True:
             pass
     except KeyboardInterrupt:
         print("Exiting...")
+    finally:
         mqtt.client.disconnect()
         mqtt.client.loop_stop()
