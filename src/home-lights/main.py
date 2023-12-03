42e46ea639827f726e9f2caebda92444cae139b5
awalford16 <awalford16@gmail.com>
2023-12-03 09:41:49 +0000

    env vars

--- src/home-lights/main.py
+++ src/home-lights/main.py
@@ -5,6 +5,7 @@ import asyncio
 from phillips import PhillipsHue
 import asyncio_mqtt as aiomqtt
 from smart_device import States
+import os
 
 FORMAT = "%(asctime)s %(levelname)s %(message)s"
 logging.basicConfig(format=FORMAT)
@@ -17,29 +18,40 @@ class Topics(Enum):
     OFFICE = "office/lights"
 
 
+MQTT_SERVER = os.environ.get("MQTT_ADDRESS", "")
+TPLINK_BULB_IP = os.environ.get("KASA_BULB_IP", "")
+
+
 async def main():
     handlers = {
-        "lounge/lights": TpLinkHandler(logger, "192.168.0.51"),
+        "lounge/lights": TpLinkHandler(logger, TPLINK_BULB_IP),
         "office/lights": PhillipsHue(logger),
     }
-    async with aiomqtt.Client("192.168.0.120") as client:
-        async with client.messages() as messages:
-            await client.subscribe(Topics.LIVING_ROOM.value)
-            await client.subscribe(Topics.OFFICE.value)
+    while True:
+        try:
+            async with aiomqtt.Client(MQTT_SERVER) as client:
+                logger.info("Connected to MQTT Server")
+                async with client.messages() as messages:
+                    await client.subscribe(Topics.LIVING_ROOM.value)
+                    await client.subscribe(Topics.OFFICE.value)
 
-            async for message in messages:
-                logger.info(
-                    f"Setting {message.topic.value} to {message.payload.decode('utf-8')}"
-                )
-                try:
-                    if message.payload == b"OFF":
-                        handlers[message.topic.value].turn_off()
-                    else:
-                        handlers[message.topic.value].turn_on(
-                            States[message.payload.decode("utf-8")]
+                    async for message in messages:
+                        logger.info(
+                            f"Setting {message.topic.value} to {message.payload.decode('utf-8')}"
                         )
-                except Exception as e:
-                    handlers[message.topic.value].log.error(f"Failed to update: {e}")
+                        try:
+                            if message.payload == b"OFF":
+                                handlers[message.topic.value].turn_off()
+                            else:
+                                handlers[message.topic.value].turn_on(
+                                    States[message.payload.decode("utf-8")]
+                                )
+                        except Exception as e:
+                            handlers[message.topic.value].log.error(
+                                f"Failed to update: {e}"
+                            )
+        except Exception as e:
+            logger.error(f"MQTT Connection error: {e}")
 
 
 if __name__ == "__main__":
